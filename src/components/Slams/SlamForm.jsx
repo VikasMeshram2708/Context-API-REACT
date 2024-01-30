eb241e8173d1b50d4bb41a7c47a452b946088be7
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-01-30 11:55:36 +0530

    :alien: Added Checked and delete functionality.

--- src/components/Slams/SlamForm.jsx
+++ src/components/Slams/SlamForm.jsx
@@ -1,13 +1,83 @@
-import React from "react";
+import { useCallback, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export default function SlamForm() {
+  const [userData, setUserData] = useState({
+    author: "",
+    message: "",
+    done: false,
+  });
+  const [slams, setSlams] = useState([]);
+
+  const handleOnChange = (event) => {
+    const { name, value } = event.target;
+    setUserData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSave = useCallback(
+    (event) => {
+      event.preventDefault();
+      console.log("user-data", userData);
+
+      if (!userData?.author) return toast.error("Author is required");
+      if (!userData?.message) return toast.error("Message is required");
+
+      setSlams([
+        ...slams,
+        {
+          id: Math.floor(1000 * Math.random() * 9000),
+          name: userData?.author,
+          content: userData?.message,
+          done: userData?.done,
+        },
+      ]);
+
+      setUserData((prev) => ({
+        ...prev,
+        author: "",
+        message: "",
+        done: "",
+      }));
+
+      console.log("user-data", userData);
+    },
+    [userData, slams]
+  );
+
+  const handleCheck = (slamId) => {
+    setSlams(
+      slams?.map((item) => {
+        if (item?.id === slamId) {
+          return {
+            ...item,
+            done: !item?.done,
+          };
+        }
+        return item;
+      })
+    );
+  };
+
+  const handleDelete = (slamId) => {
+    let storedSlams = slams?.filter((item) => item?.id !== slamId);
+    setSlams(storedSlams);
+  };
+
+  useEffect(() => {}, [userData, slams]);
   return (
     <section className="max-w-md mx-auto">
-      <form className="bg-slate-500 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form
+        id="slamForm"
+        onSubmit={handleSave}
+        className="bg-slate-500 shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
         <div className="mb-4">
           <label
             htmlFor="author"
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-white text-sm font-bold mb-2"
           >
             Author Name
           </label>
@@ -15,6 +85,8 @@ export default function SlamForm() {
             type="text"
             name="author"
             id="author"
+            value={userData?.author}
+            onChange={handleOnChange}
             placeholder="Enter author name"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
@@ -23,7 +95,7 @@ export default function SlamForm() {
         <div className="mb-6">
           <label
             htmlFor="message"
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="block text-white text-sm font-bold mb-2"
           >
             Message
           </label>
@@ -31,6 +103,8 @@ export default function SlamForm() {
             type="text"
             name="message"
             id="message"
+            value={userData?.message}
+            onChange={handleOnChange}
             placeholder="Enter message"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
@@ -45,6 +119,55 @@ export default function SlamForm() {
           </button>
         </div>
       </form>
+
+      <ul className="flex flex-col gap-2">
+        {slams?.map((item) => {
+          return (
+            <div
+              className="bg-gray-500 flex justify-between gap-2 py-2 px-3 rounded-md"
+              key={item?.id}
+            >
+              <div className={`flex items-center gap-2`}>
+                <input
+                  type="checkbox"
+                  name="done"
+                  id="done"
+                  checked={userData?.checked}
+                  onChange={(e) =>
+                    setUserData((prev) => ({
+                      ...prev,
+                      done: e.target.checked,
+                    }))
+                  }
+                  onClick={() => handleCheck(item?.id)}
+                />
+                <p
+                  id="userContent"
+                  className={`${item?.done ? "line-through" : ""}`}
+                >
+                  {item?.content}
+                </p>
+                <h1>{item?.name}</h1>
+              </div>
+              <div className="flex items-center gap-2">
+                <button
+                  type="button"
+                  className="bg-green-500 text-white font-semibold text-sm p-2 rounded-lg"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => handleDelete(item?.id)}
+                  type="button"
+                  className="bg-red-500 text-white font-semibold text-sm p-2 rounded-lg"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </ul>
     </section>
   );
 }
